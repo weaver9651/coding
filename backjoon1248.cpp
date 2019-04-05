993f76cd85526d8d3e02b525df88a77d27cd97cc
weaver9651 <weaver9651@naver.com>
2019-04-05 21:21:23 +0900

    backjoon1248 completed

--- backjoon1248.cpp
+++ backjoon1248.cpp
@@ -30,9 +30,11 @@ bool check(int index) {
     sum += ans[i];
     if (sign[i][index] == 0) {
       if (sum != 0) return false;
-    } else if (sign[i][index] < 0) {
+    }
+    else if (sign[i][index] < 0) {
       if (sum >= 0) return false;
-    } else if (sign[i][index] < 0) {
+    }
+    else if (sign[i][index] < 0) {
       if (sum <= 0) return false;
     }
   }
@@ -42,25 +44,30 @@ bool check(int index) {
 
 // && 연산 시 check를 앞에 둠으로써 만약 check에서 false가 나면 뒤는 자동으로 계산하지 않음.
 bool go(int index) {
+  // 마지막 index를 넘어서면 정상종료
   if (index == N) return true;
+
+  // sign이 0인 경우는 무조건 0을 할당하고 종료(다음 index의 정상종료값을 찾음)
   if (sign[index][index] == 0) {
-      ans[index] = 0;
-      return check(index) && go(index + 1);
-    }
-  for (int i = 1; i<= 10; i++) {
+    ans[index] = 0;
+    return check(index) && go(index + 1);
+  }
+
+  // sign이 0이 아니면 1부터 숫자를 하나씩 넣어가면서 다음 index를 찾음(DFS)
+  for (int i = 1; i <= 10; i++) {
     ans[index] = sign[index][index] * i;
     if (check(index) && go(index + 1)) return true;
   }
   return false;
 }
 
-int main () {
+int main() {
   In();
   go(0);
   for (int i = 0; i < N; i++) {
     cout << ans[i] << " ";
   }
   cout << endl;
-  
+
   return 0;
 }
