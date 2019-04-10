c99ffae2a299db3c35763de1a8062f0673fa9075
weaver9651 <weaver9651@naver.com>
2019-04-10 13:04:38 +0900

    swea7338.cpp timeout

--- swea7338.cpp
+++ swea7338.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
-#include <vector>
+#include <cstdio>
 
 using namespace std;
 
 int main () {
   int T;
   cin >> T;
-  vector<pair<long long, int> > results;
   long long Y;
   int M;
   long long newYear;
   int newMonth;
   const int  baseYear = 2016;
-  
+
   for (int tc = 0; tc < T; tc++) {
-    cin >> Y >> M;
+    scanf("%lld", &Y);
+    scanf("%d", &M);
     long long tot = (Y - baseYear)*12 + M;
     newYear = (tot-1) / 13 + baseYear;
     newMonth = (tot-1) % 13 + 1;
-    results.push_back(make_pair(newYear, newMonth));
+
+    printf("#%d %lld %d\n", tc+1, newYear, newMonth);
   }
 
-  int i = 0;
-  for (vector<pair<long long, int> >::iterator iter = results.begin(); iter != results.end(); iter++) {
-    i++;
-    cout << "#" << i << " " << iter->first << " " << iter->second << endl;
-  }
-  
   return 0;
 }
