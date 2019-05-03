c428590247334a09358066762bff83b483c14d49
weaver9651 <weaver9651@naver.com>
2019-05-03 19:18:40 +0900

    pathfinding_baekjoon11403.cpp solving

---
+++ pathfinding_baekjoon11403.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstdio>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int edge[100][100];
+bool visit[100];
+int Map[100][100];
+
+void In() {
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      scanf("%d", &edge[i][j]);
+    }
+  }
+}
+
+void init() {
+  for (int i = 0; i < N; i++) {
+    visit[i] = false;
+  }
+}
+
+bool bfs(int src, int dst) {
+  queue<int> q;
+  q.push(src);
+  int cur, tmp;
+
+  while (!q.empty()) {
+    cur = q.front();
+    q.pop();
+
+    if (cur == dst) {
+      init();
+      return true;
+    }
+
+    for (int i = 0; i < N; i++) {
+      if (i == cur)
+	continue;
+      if (edge[cur][i] && !visit[i]) {
+	q.push(i);
+	visit[i] = true;
+      }
+    }
+  }
+  init();
+  return false;
+}
+
+void Out() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (bfs(i, j))
+	printf("%d ", 1);
+      else
+	printf("%d ", 0);
+    }
+    cout << endl;
+  }
+}
+
+int main () {
+  In();
+  Out();
+  
+  return 0;
+}
