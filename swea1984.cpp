e4e267a1c078b56af742f03d7d8adde4c9185dd2
weaver9651 <weaver9651@naver.com>
2019-05-15 17:55:55 +0900

    swea1984.cpp solving

---
+++ swea1984.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  int T;
+  scanf("%d", &T);
+  for (int tc = 0; tc < T; tc++) {
+    int tmp;
+    int maxx = 0;
+    int minn = 10000;
+    long long sum = 0;
+    for (int i = 0; i < 10; i++) {
+      scanf("%d", &tmp);
+      sum += tmp;
+      if (minn > tmp)
+	minn = tmp;
+      if (maxx < tmp)
+	maxx = tmp;
+    }
+    sum = sum - minn - maxx;
+    printf("#%d %d\n", tc+1, sum / 8);
+  }
+  
+  return 0;
+}
