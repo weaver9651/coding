ac54b28ac5e033d695babbd7bafdfd7ec1945c30
weaver9651 <weaver9651@naver.com>
2019-05-21 16:34:36 +0900

    swea1824.cpp solving

---
+++ swea1824.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+char Map[20][20];
+int state[20][20];
+int N, M;
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+int dir;
+int mem;
+bool isExit;
+int cur_y, cur_x;
+
+void In() {
+  char tmp;
+  scanf("%d%d", &N, &M);
+  scanf("%c", &tmp);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      scanf("%c", &Map[i][j]);
+      state[i][j] = 0;
+    }
+    scanf("%c", &tmp);
+  }
+  dir = 1;
+  mem = 0;
+  isExit = false;
+  cur_y = 0;
+  cur_x = 0;
+}
+
+void action(char x) {
+  if ('0' <= x && x <= '9') {
+    mem = x - 48;
+  }
+  else {
+    switch(x) {
+    case '<':
+      dir = 3;
+      break;
+    case '>':
+      dir = 1;
+      break;
+    case '^':
+      dir = 0;
+      break;
+    case 'v':
+      dir = 2;
+      break;
+    case '_':
+      if (mem == 0) {
+	dir = 1;
+      }
+      else {
+	dir = 3;
+      }
+      break;
+    case '|':
+      if (mem == 0) {
+	dir = 2;
+      }
+      else {
+	dir = 0;
+      }
+      break;
+    case '.':
+      break;
+    case '@':
+      isExit = true;
+      break;
+    case '+':
+      mem++;
+      break;
+    case '-':
+      mem--;
+      break;
+    case '?':
+      dir = rand() % 4;
+      break;
+    }
+  }
+  
+  cur_y = (cur_y + dy[dir]) % N;
+  cur_x = (cur_x + dx[dir]) % M;
+  
+  return;
+}
+
+void Out() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      printf("%c", Map[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+int main () {
+  srand(static_cast<unsigned int>(time(0)));
+
+  In();
+  int counter = 0;
+  while (!isExit) {
+    action(Map[cur_y][cur_x]);
+    counter++;
+    if (isExit) {
+      printf("YES\n");
+    }
+    
+    if (counter == 1000) {
+      printf("NO\n");
+      break;
+    }
+    
+  }
+  
+  return 0;
+}
