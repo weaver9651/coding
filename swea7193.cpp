a311c50f5bed0ee72d1b1212ec8c6c7e88822fc8
weaver9651 <weaver9651@naver.com>
2019-05-23 18:48:47 +0900

    swea7193.cpp solving 4

--- swea7193.cpp
+++ swea7193.cpp
@@ -8,13 +8,14 @@ int NtoDec(int N, long long X) {
   long long result = 0;
   int counter = 0;
   while (X) {
-    result += (X % 10) * pow(N, counter);
+    result += (long long)((X % 10) * pow(N, counter)) % (N-1);
     X /= 10;
     counter++;
   }
   
-  return result;
+  return result % (N-1);
 }
+
 
 int main () {
   int T;
@@ -23,9 +24,9 @@ int main () {
     int N;
     long long X;
     cin >> N >> X;
-    long long result = 0;
-    result = NtoDec(N, X) % (N-1);
-    printf("#%d %lld\n", tc+1, result);
+    int result = 0;
+    result = NtoDec(N, X);
+    printf("#%d %d\n", tc+1, result);
   }
   
   return 0;
