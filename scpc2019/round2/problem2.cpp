122a8796f317578a4aac49903d84e78d69c8220e
weaver9651 <weaver9651@naver.com>
2019-07-06 20:32:08 +0900

    problem2.cpp solving 4

--- scpc2019/round2/problem2.cpp
+++ scpc2019/round2/problem2.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 
 int N;
+int rightadd[5000];
+int leftadd[5000];
 
 int *flipArr(int *arr, int start, int end) {
   int *result = new int[N];
@@ -28,38 +30,53 @@ int getPoint(int *arrA, int *arrB, int maxx) {
   return counter;
 }
 
-bool isPlus(int *arrA, int *arrB, int start, int end) {
+int getPoint2(int *arrA, int *arrB, int start, int end) {
   int cur = 0;
   int next = 0;
+  int result = 0;
   for (int i = 0; i <= end-start; i++) {
     if (arrA[start+i] == arrB[start+i])
       cur++;
     if (arrA[start+i] == arrB[end-i])
       next++;
   }
-  if (next > cur)
-    return true;
-  else
-    return false;
+  if (start != 0) {
+    result += rightadd[start-1];
+  }
+  if (end != N-1) {
+    result += leftadd[end+1];
+  }
+  result += next;
+  return result;
 }
 
 int findAnswer(int *arrA, int *arrB) {
-  int maxx = 0;
-  maxx = getPoint(arrA, arrB, maxx);
+  int maxx = leftadd[0];
   for (int i = 0; i < N-1; i++) {
     for (int j = i+1; j < N; j++) {
       int cur_score = 0;
-      if (isPlus(arrA, arrB, i, j)) {
-	int *tmp = flipArr(arrB, i, j);
-	cur_score = getPoint(arrA, tmp, maxx);
-	if (cur_score > maxx) {
-	  maxx = cur_score;
-	}
-	delete [] tmp;
+      cur_score = getPoint2(arrA, arrB, i, j);
+      if (cur_score > maxx) {
+	maxx = cur_score;
       }
     }
   }
   return maxx;
+}
+
+void accumulate(int *arrA, int *arrB) {
+  int counterright = 0;
+  int counterleft = 0;
+  for (int i = 0; i < N; i++) {
+    if (arrA[i] == arrB[i]) {
+      counterright++;
+    }
+    rightadd[i] = counterright;      
+    if (arrA[N-i-1] == arrB[N-i-1]) {
+      counterleft++;
+    }
+    leftadd[N-i-1] = counterleft;
+  }
 }
 
 int main () {
@@ -75,6 +92,8 @@ int main () {
     for (int i = 0; i < N; i++) {
       scanf("%d", &arrB[i]);
     }
+
+    accumulate(arrA, arrB); 
 
     int result = findAnswer(arrA, arrB);
     printf("Case #%d\n%d\n", tc, result);
