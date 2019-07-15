31b505ab6fed437f2d7e999db98791bf8a541f7a
weaver9651 <weaver9651@naver.com>
2019-07-15 14:27:35 +0900

    baekjoon2579.cpp solving 3

--- baekjoon2579.cpp
+++ baekjoon2579.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int arr[300];
-int tot[300][3];
+int tot[300][4];
 int N;
 
 void In() {
@@ -42,18 +42,21 @@ int findMax() {
   tot[1][0] = arr[0];
   tot[1][1] = arr[0] + arr[1];
   tot[1][2] = arr[0] + arr[1];
+  tot[1][3] = arr[0] + arr[1];
+  
   if (N == 2)
     return tot[1][1];
   
   tot[2][0] = tot[1][2];
-  tot[2][1] = tot[0][0] + arr[2];
+  tot[2][1] = tot[1][0] + arr[2];
   tot[2][2] = tot[1][2] + arr[2];
- 
+  tot[2][3] = tot[1][1];
   
   for (int i = 3; i < N; i++) {
     tot[i][0] = tot[i-1][2];
-    tot[i][1] = tot[i-1][0] + arr[i];
+    tot[i][1] = max(tot[i-1][0] + arr[i], tot[i-1][3] + arr[i]);
     tot[i][2] = tot[i-1][1] + arr[i];
+    tot[i][3] = tot[i-1][1];
   }
     return max(tot[N-1][1], tot[N-1][2]);
 }
