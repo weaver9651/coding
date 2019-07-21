d2d6ac91249ad2abe68bb64dc8897cb24d7537bd
weaver9651 <weaver9651@naver.com>
2019-07-21 15:24:28 +0900

    baekjoon11726.cpp solving 2

--- baekjoon11726.cpp
+++ baekjoon11726.cpp
@@ -7,7 +7,7 @@ int findAns(int N) {
   arr[1] = 1;
   arr[2] = 2;
   for (int i = 3; i <= N; i++) {
-    arr[i] = arr[i-1] + arr[i-2];
+    arr[i] = (arr[i-1] + arr[i-2]) % 10007;
   }
   return arr[N];
 }
@@ -15,7 +15,7 @@ int findAns(int N) {
 int main () {
   int N;
   scanf("%d", &N);
-  int result = findAns(N) % 10007;
+  int result = findAns(N);
   printf("%d\n", result);
   
   return 0;
