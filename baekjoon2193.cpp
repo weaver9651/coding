769e8463b5f7a7c56282daa125579e97dbf20a5e
weaver9651 <weaver9651@naver.com>
2019-07-21 16:07:43 +0900

    baekjoon2193.cpp solving 2

--- baekjoon2193.cpp
+++ baekjoon2193.cpp
@@ -10,14 +10,18 @@ int findAns(int N) {
   
   arr[1][0] = 0;
   arr[1][1] = 1;
-  arr[2][0] = 0;
-  arr[2][1] = 1;
+  if (N == 1)
+    return arr[N][0] + arr[N][1];
+  arr[2][0] = 1;
+  arr[2][1] = 0;
+  if (N == 2)
+    return arr[N][0] + arr[N][1];
 
   for (int i = 3; i <= N; i++) {
     arr[i][0] = arr[i-1][1] + arr[i-1][0];
     arr[i][1] = arr[i-1][0];
   }
-  return arr[N][0]*2 + arr[N][1];
+  return arr[N][0] + arr[N][1];
 }
 
 int main () {
