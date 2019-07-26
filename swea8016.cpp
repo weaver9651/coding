6c60a722a9982a7627ae6f97983814cf9daeaa3b
Giyeol Kim <gykim@sor.snu.ac.kr>
2019-07-26 22:27:51 +0900

    swea8016.cpp solving 6

--- swea8016.cpp
+++ swea8016.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
-typedef unsigned long long ull;
-
 using namespace std;
 
 int main () {
-  int T, N;
+  int T;
   cin >> T;
+  unsigned long long N;
   for (int tc = 1; tc <= T; tc++) {
     cin >> N;
-    ull left = (N-1)*(N-1)*2 + 1;
-    ull right = 2*N*N - 1;
+    unsigned long long left, right;
+    left = (N-1)*(N-1)*2 + 1;
+    right = 2*N*N - 1;
     cout << "#" << tc << " " << left << " " << right << endl;
   }
   
