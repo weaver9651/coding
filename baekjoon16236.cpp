e77fd295bea1d7395791b72405efb575cb4e34a7
Giyeol Kim <gykim@sor.snu.ac.kr>
2019-08-05 10:13:06 +0900

    baekjoon16236.cpp solving

---
+++ baekjoon16236.cpp
@@ -0,0 +1,158 @@
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+/*
+크기가 같으면 지나가기만 가능
+*/
+
+int Map[20][20];
+int dist[20][20] = {0,};
+int shark_y, shark_x;
+int dy[] = {0, 0, 1, -1};
+int dx[] = {1, -1, 0, 0};
+bool visit[20][20] = {false, };
+int level = 2;
+int exp = 0;
+int N;
+
+bool isInside(int y, int x) {
+  if (y >= 0 && y < N && x >= 0 && x < N)
+    return true;
+  else
+    return false;
+}
+
+void In() {
+  scanf("%d", &N);
+  int tmp;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      scanf("%d", &tmp);
+      Map[i][j] = tmp;
+      if (tmp == 9) {
+	shark_y = i;
+	shark_x = j;
+	Map[i][j] = 0;
+      }
+    }
+  }
+}
+
+void bfs(int start_y, int start_x) {
+  int cur_y, cur_x;
+  int tmp_y, tmp_x;
+  queue<pair<int, int> > q;
+  queue<pair<int, int> > buffer;
+  vector<pair<int, int> > candidates;
+  q.push(make_pair(start_y, start_x));
+  visit[start_y][start_x] = true;
+  
+  while (!q.empty() || !buffer.empty()) {
+    if (!candidates.empty()) {
+      int minny = 1000;
+      int minnx = 1000;
+      int index = 0;
+      int counter = 0;
+      for (vector<pair<int, int> >::iterator it = candidates.begin();
+	   it != candidates.end(); it++) {
+	counter++;
+	if (it->first < minny) {
+	  minny = it->first;
+	  minnx = it->second;
+	  index = counter;
+	}
+	else if (it->first == minny) {
+	  if (it->second < minnx) {
+	    minnx = it->second;
+	    index = counter;
+	  }
+	}
+      }
+      tmp_y = candidates[index-1].first;
+      tmp_x = candidates[index-1].second;
+      int tmp = Map[tmp_y][tmp_x];
+      Map[tmp_y][tmp_x] = 0;
+      exp++;
+      if (exp >= level) {
+	exp = 0;
+	level++;
+      }
+      shark_y = tmp_y;
+      shark_x = tmp_x;
+      return;
+    }
+
+    while (!buffer.empty()) {
+      q.push(buffer.front());
+      buffer.pop();
+    }
+
+    while (!q.empty()) {        
+      cur_y = q.front().first;
+      cur_x = q.front().second;
+      q.pop();
+
+      for (int i = 0; i < 4; i++) {
+	tmp_y = cur_y + dy[i];
+	tmp_x = cur_x + dx[i];
+	if (isInside(tmp_y, tmp_x) && Map[tmp_y][tmp_x] <= level && !visit[tmp_y][tmp_x]) {
+	  visit[tmp_y][tmp_x] = true;
+	  buffer.push(make_pair(tmp_y, tmp_x));
+	  dist[tmp_y][tmp_x] = dist[cur_y][cur_x] + 1;
+	  if (Map[tmp_y][tmp_x] < level && Map[tmp_y][tmp_x] > 0) {
+	    candidates.push_back(make_pair(tmp_y, tmp_x));
+	  }
+	}
+      }
+    }
+  } // while q buffer end
+}
+
+void init() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      visit[i][j] = false;
+    }
+  }
+}
+
+bool isFood() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (Map[i][j] < level && Map[i][j] > 0)
+	return true;
+    }
+  }
+  return false;
+}
+
+void Out() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      printf("%d ", Map[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+int findAns() {
+  while(isFood()) {
+    bfs(shark_y, shark_x);
+    //    Out();
+    init();
+  }
+  return dist[shark_y][shark_x];
+}
+
+int main () {
+  In();
+  int result = findAns();
+  printf("%d\n", result);
+  
+  return 0;
+}
