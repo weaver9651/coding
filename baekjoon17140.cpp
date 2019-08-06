739d8d077258fbf80d4fcf032220124cf736ac56
Giyeol Kim <gykim@sor.snu.ac.kr>
2019-08-06 19:43:35 +0900

    baekjoon17140.cpp solving

--- baekjoon17140.cpp
+++ baekjoon17140.cpp
@@ -4,59 +4,35 @@
 
 using namespace std;
 
-/*
-행의 갯수가 많거나 같으면 R연산, 모든 행에 대해서 정렬
-행의 갯수가 적으면 C연산, 모든 열에 대해서 정렬
-
-숫자 갯수가 작은 순서대로(pair->second)
-그 안에서는 숫자가 작은 순서대로(pair->first)
-
-나머지는 0으로 채우고 정렬한 결과를 낼 때 0은 무시
-100개를 넘어가면 그 이후는 버림
-
-최초에는 3x3 행렬
-*/
-
+int R, C, K;
 int Map[101][101] = {0, };
-int r, c, k;
-int cur_r = 3, cur_c = 3;
+int max_column = 0, max_row = 0;
 
 void In() {
-  scanf("%d%d%d", &r, &c, &k);
+  scanf("%d%d%d", &R, &C, &K);
   for (int i = 1; i <= 3; i++) {
-    Map[i][0] = 3;
-    Map[0][i] = 3;
     for (int j = 1; j <= 3; j++) {
       scanf("%d", &Map[i][j]);
     }
   }
-}
-
-bool sortbysec(const pair<int,int> &a, const pair<int,int> &b) {
-  return (a.second < b.second);
-}
-
-bool isInside(int y, int x) {
-  if (y >= 1 && x <= cur_r && x >= 1 && x <= cur_c)
-    return true;
-  else
-    return false;
+  max_column = 3;
+  max_row = 3;
 }
 
 int numOfNum(int y, int x, char dir) {
-  int tmp = Map[y][x];
   int counter = 0;
-  if (dir == 'r') {
-    for (int i = 1; i <= 100; i++) {
-      if (tmp == Map[y][i]) {
+  int num = Map[y][x];
+  if (dir == 'r') { // change column
+    for (int i = x; i <= max_column; i++) {
+      if (Map[y][i] == num) {
 	counter++;
 	Map[y][i] = 0;
       }
     }
   }
-  else { /* dir == 'c' */
-    for (int i = 1; i <= 100; i++) {
-      if (tmp == Map[i][x]) {
+  else { /* dir == 'c' */ // change row
+    for (int i = y; i <= max_row; i++) {
+      if (Map[i][x] == num) {
 	counter++;
 	Map[i][x] = 0;
       }
@@ -65,84 +41,92 @@ int numOfNum(int y, int x, char dir) {
   return counter;
 }
 
-void calcR() {
-  int tmp;
-  for (int i = 1; i <= 100; i++) {
-    vector<pair<int, int> > s;
-    for (int j = 1; j <= 100; j++) {
+bool sortbysec(const pair<int,int> &a, const pair<int,int> &b) {
+  return (a.second < b.second);
+}
+
+void calR() {
+  int num, cnt, cur_size;
+  int tmp_column = 0;
+  for (int i = 1; i <= max_row; i++) {
+    vector<pair<int,int> > s;
+    for (int j = 1; j <= max_column; j++) {
       if (Map[i][j] != 0) {
-	int num = Map[i][j];
-	tmp = numOfNum(i, j, 'r');
-	s.push_back(make_pair(num, tmp));
+	num = Map[i][j];
+	cnt = numOfNum(i, j, 'r');
+	s.push_back(make_pair(num, cnt));
       }
     }
+    /* locate numbers from s */
+    cur_size = s.size() * 2;
+    if (cur_size > 100)
+      cur_size = 100;
+    tmp_column = max(tmp_column, cur_size);
+
     sort(s.begin(), s.end());
     sort(s.begin(), s.end(), sortbysec);
-    
-    Map[i][0] = s.size() * 2;
-    if (Map[i][0] > 100)
-      Map[i][0] = 100;
-    cur_c = max(cur_c, Map[i][0]);
-    
+
     vector<pair<int,int> >::iterator it = s.begin();
-    for (int j = 1; j <= Map[i][0]; /* none */) {
+    for (int j = 1; j <= cur_size; /* none */) {
       Map[i][j] = it->first;
       Map[i][j+1] = it->second;
-      it++;
       j += 2;
-    }
+      it++;
+    }        
   }
+  max_column = tmp_column;
 }
 
-void calcC() {
-  int tmp;
-  for (int j = 1; j <= 100; j++) {
-    vector<pair<int, int> > s;
-    for (int i = 1; i <= 100; i++) {
-      if ((Map[i][j] != 0)) {
-	int num = Map[i][j];
-	tmp = numOfNum(i, j, 'c');
-	s.push_back(make_pair(num, tmp));
-	Map[i][j] = 0;
+void calC() {
+  int num, cnt, cur_size;
+  int tmp_row = 0;
+  for (int j = 1; j <= max_column; j++) {
+    vector<pair<int,int> > s;
+    for (int i = 1; i <= max_row; i++) {
+      if (Map[i][j] != 0) {
+	num = Map[i][j];
+	cnt = numOfNum(i, j, 'c');
+	s.push_back(make_pair(num, cnt));
       }
     }
+    /* locate numbers from s */
+    cur_size = s.size() * 2;
+    if (cur_size > 100)
+      cur_size = 100;
+    tmp_row = max(tmp_row, cur_size);
+
     sort(s.begin(), s.end());
     sort(s.begin(), s.end(), sortbysec);
 
-    Map[0][j] = s.size() * 2;
-    if (Map[0][j] > 100)
-      Map[0][j] = 100;
-    cur_r = max(cur_r, Map[0][j]);
-    vector<pair<int, int> >::iterator it = s.begin();
-    for (int i = 1; i <= Map[0][j]; /* none */) {
+    vector<pair<int,int> >::iterator it = s.begin();
+    for (int i = 1; i <= cur_size; /* none */) {
       Map[i][j] = it->first;
       Map[i+1][j] = it->second;
-      it++;
       i += 2;
-    }
+      it++;
+    }        
   }
+  max_row = tmp_row;
 }
 
-void calc() {
-  if (cur_r >= cur_c) {
-    calcR();
+void cal() {
+  if (max_row >= max_column) {
+    calR();
   }
   else {
-    calcC();
+    calC();
   }
 }
 
 int findAns() {
   int counter = 0;
-  while (!(Map[r][c] == k)) {
-    calc();
+  while(Map[R][C] != K) {
     counter++;
     if (counter > 100)
       return -1;
+    cal();
   }
-  if (Map[r][c] == k) {
-    return counter;
-  }
+  return counter;
 }
 
 int main () {
