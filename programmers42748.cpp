0a64175cc79a6996649740f41120a6235d19932b
Giyeol Kim <gykim@sor.snu.ac.kr>
2019-11-14 20:45:58 +0900

    programmers42748.cpp solving 2

--- programmers42748.cpp
+++ programmers42748.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -10,12 +11,26 @@ vector<int> solution(vector<int> array, vector<vector<int> > commands) {
   for (auto command : commands) {
     i = command[0] - 1;
     j = command[1] - 1;
-    k = command[2];
+    k = command[2] - 1;
     vector<int> tmp;
     vector<int>::iterator it = array.begin();
-    copy(it + i, it + j, tmp.begin());
+    tmp.resize(j - i + 1);
+    copy(it + i, it + j + 1, tmp.begin());
     sort(tmp.begin(), tmp.end());
     answer.push_back(tmp[k]);
   }
   return answer;
+}
+
+int main () {
+  vector<int> array = {1, 5, 2, 6, 3, 7, 4};
+  vector<vector<int> > commands = { {2, 5, 3},
+				    {4, 4, 1},
+				    {1, 7, 3} };
+  vector<int> result;
+  result = solution(array, commands);
+  for (auto x : result)
+    cout << x << endl;
+  
+  return 0;
 }
